09a7f60290248af1ef9c97ad4bd9080052fd4e02
Sugam Garg <sgarg6@nlp-gpu-01.soe.ucsc.edu>
2023-01-28 14:11:13 -0800

    Added Bi-Direction option

--- rnnlm.py
+++ rnnlm.py
@@ -12,6 +12,7 @@ class RNNModel(nn.Module):
         n_layers,
         dropout=0.5,
         rnn_type="elman",  # can be elman, lstm, gru
+        bi_direction=False,
     ):
         super(RNNModel, self).__init__()
 
@@ -22,6 +23,7 @@ class RNNModel(nn.Module):
                 n_layers,
                 nonlinearity="tanh",
                 dropout=dropout,
+                bidirectional=bi_direction,
             )
         elif rnn_type == "gru":
             self.rnn = nn.GRU(
@@ -29,23 +31,26 @@ class RNNModel(nn.Module):
                 n_hidden,
                 n_layers,
                 dropout=dropout,
+                bidirectional=bi_direction,
             )
         elif rnn_type == "lstm":
             self.rnn = nn.LSTM(
                 in_embedding_dim,
                 n_hidden,
                 n_layers,
-                dropout=dropout
+                dropout=dropout,
+                bidirectional=bi_direction,
             )
         
         self.in_embedder = nn.Embedding(vocab_size, in_embedding_dim)
         self.dropout = nn.Dropout(dropout)
-        self.pooling = nn.Linear(n_hidden, vocab_size)
+        self.pooling = nn.Linear(n_hidden * 2, vocab_size) if bi_direction else nn.Linear(n_hidden, vocab_size)
         self.init_weights()
         self.n_hidden = n_hidden
         self.n_layers = n_layers
         self.vocab_size = vocab_size
         self.rnn_type = rnn_type
+        self.bi_direction = bi_direction
 
     def init_weights(self):
         initrange = 0.1
@@ -63,12 +68,13 @@ class RNNModel(nn.Module):
 
     def init_hidden(self, batch_size):
         weight = next(self.parameters())
+        first_dim = self.n_layers * 2 if self.bi_direction else self.n_layers
         if self.rnn_type == "lstm":
             return (
-                    weight.new_zeros(self.n_layers, batch_size, self.n_hidden), 
-                    weight.new_zeros(self.n_layers, batch_size, self.n_hidden)
+                    weight.new_zeros(first_dim, batch_size, self.n_hidden), 
+                    weight.new_zeros(first_dim, batch_size, self.n_hidden)
                     )
-        return weight.new_zeros(self.n_layers, batch_size, self.n_hidden)
+        return weight.new_zeros(first_dim, batch_size, self.n_hidden)
 
     @staticmethod
     def load_model(path):
